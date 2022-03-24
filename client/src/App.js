83d85d799c02633ce84f86edad230e2056c305d0
Jasmin Halkic <jasmin.halkic0@gmail.com>
2022-03-24 11:50:57 -0400

    initial share

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,120 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  
+  const [messageData, setMessageData] = useState([]);
+
+  const [input, setInput] = useState({ user: "" }, { messageBody: "" });
+
+  function handleChange(event) {
+    let newInput = { ...input, [event.target.name]: event.target.value };
+    setInput(newInput);
+  }
+
+  async function handleSubmit() {
+    await fetch("http://localhost:8000/add-message", {
+      headers: { "content-type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(input),
+    });
+  }
+
+  async function getData() {
+    let allMessages = await fetch("http://localhost:8000/get-all-messages");
+    allMessages = await allMessages.json();
+    setMessageData(allMessages);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello, Chat!</h1>
-    </div>
+
+    <body>
+
+      <header id="header">
+        <h1>Yooooo</h1>
+        <hr></hr>
+        <hr></hr>
+      </header>
+
+
+      <main id="bodycontainer">
+        
+
+      <div id="leftscreen">
+          <div id="rooms">
+
+            <h3>Available Rooms</h3>
+            
+            <div id="roomselection">jkh</div>
+            <div id = "adspace">ADS</div>
+          </div>
+        </div>
+    
+
+
+        <div id="rightscreen">
+          <div id="currentroom">
+          <h3>Current Room</h3>
+          </div>
+          </div>
+
+
+
+          <div id="chatbox">
+          {messageData.map((message) => {
+              return (
+                <>
+                  <h1>----------</h1>
+                  <h2>Username: {message.user}</h2>
+                  <p>Message: {message.body}</p>
+                  <h1>----------</h1>
+                </>
+              );
+            })}
+          </div>
+
+          <div id="input-forms">
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username:
+              <input
+                type="text"
+                name="user"
+                value={input.user}
+                onChange={handleChange}
+              />
+            </label>
+
+            <label>
+              Message:
+              <input
+                type="text"
+                name="messageBody"
+                value={input.messageBody}
+                onChange={handleChange}
+              />
+            </label>
+
+            <button type="submit" value="Submit">
+              Submit
+            </button>
+
+            <button type="submit" value="Submit">
+              Refresh
+            </button>
+
+          </form>
+          </div>
+
+          </main>
+      <footer></footer>
+
+    </body>
+
   );
 }
 
--- server.js
+++ server.js
@@ -2,11 +2,52 @@ const express = require("express");
 
 const port = process.env.PORT || 8000;
 const app = express();
+const cors = require("cors");
 
-const Message = require('./Message.js')
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/messages");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+
+const messageSchema = new mongoose.Schema({
+  when: Date,
+  user: String,
+  room: String,
+  messageBody: String,
+});
+
+const Message = mongoose.model("messages", messageSchema);
 
 
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+app.get("/get-all-messages", async (req, res) => {
+  let allMessages = await Message.find({});
+  res.send(allMessages);
+});
+
+function addMessage(){
+  let message = new Message({user: "Max", messageBody: "What's up"});
+  message.save();
+}
+
+addMessage();
+
+async function read(){
+  let allMessages = await Message.find({});
+  console.log(allMessages)
+}
+
+read()
+
+app.post("/add-message", async (req, res) => {
+  let message = new Message(req.body);
+  message.save();
+});
 
 app.listen(port, () => {
-  console.log('listening on port: ' + port) 
-})
+  console.log("Now listening on http://localhost:" + port);
+});
