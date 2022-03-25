80405329f4ae2c31894ff75894e9692fae7121f5
Jasmin Halkic <jasmin.halkic0@gmail.com>
2022-03-24 20:16:44 -0400

    fixed + 2 iceboxes complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,120 +1,165 @@
 import React, { useState, useEffect } from "react";
+import Picture from "./images/market.png";
+import Picture2 from "./images/market2.jpg";
+import Ping from "./audio/ping.mp3";
+import ChatIcon from "./images/chat-icon.png"
 
 import "./App.css";
 
 function App() {
-  
   const [messageData, setMessageData] = useState([]);
-
-  const [input, setInput] = useState({ user: "" }, { messageBody: "" });
+  const [currentRoom, setCurrentRoom] = useState("Main Room");
+  const [input, setInput] = useState(
+    { user: "" },
+    { messageBody: "" },
+    { room: "" }
+  );
 
   function handleChange(event) {
-    let newInput = { ...input, [event.target.name]: event.target.value };
+    let newInput = {
+      ...input,
+      [event.target.name]: event.target.value,
+      room: currentRoom,
+    };
     setInput(newInput);
   }
 
-  async function handleSubmit() {
+  async function handleSubmit(event) {
+    let clearInput = { ...input, messageBody: "" };
+    setInput(clearInput);
+    event.preventDefault();
+    let audio = new Audio(Ping);
+    audio.play();
+    let checkInputArray = input.messageBody.toString().split("")
+    if (checkInputArray.length < 500) {
     await fetch("http://localhost:8000/add-message", {
       headers: { "content-type": "application/json" },
       method: "POST",
       body: JSON.stringify(input),
     });
+    } else {
+      alert("Woah woah woah, take it easy mate! 500 or less characters!")
+    }
   }
 
   async function getData() {
+    let allMessagesArray = [];
     let allMessages = await fetch("http://localhost:8000/get-all-messages");
     allMessages = await allMessages.json();
-    setMessageData(allMessages);
+    allMessages.forEach((message) => {
+      allMessagesArray.push(message);
+    });
+    setMessageData(allMessagesArray.reverse());
   }
+
   useEffect(() => {
-    getData();
+    const fetchNewData = () => {
+      getData();
+      setTimeout(fetchNewData, 0);
+    };
+    setTimeout(fetchNewData, 0);
   }, []);
 
+  let images = [Picture, Picture2];
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (currentImage === images.length - 1) {
+        setCurrentImage(0);
+      } else {
+        setCurrentImage(currentImage + 1);
+      }
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, [currentImage, images.length]);
+
   return (
-
-    <body>
-
+    <main>
       <header id="header">
-        <h1>Yooooo</h1>
-        <hr></hr>
-        <hr></hr>
+        <h1>React Chat</h1>
+        <img src={ChatIcon} alt="some chat bubbles" height="50px" />
       </header>
-
-
-      <main id="bodycontainer">
-        
-
-      <div id="leftscreen">
+      <hr></hr>
+        <hr></hr>
+      <div id="body-container">
+        <div id="left-screen">
           <div id="rooms">
-
             <h3>Available Rooms</h3>
-            
-            <div id="roomselection">jkh</div>
-            <div id = "adspace">ADS</div>
+            <div id="room-container">
+              <ul id="room-selection">
+                <li onClick={() => setCurrentRoom("Main Room")}>Main Room</li>
+                <li onClick={() => setCurrentRoom("Pets")}>Pets</li>
+                <li onClick={() => setCurrentRoom("Food")}>Food</li>
+              </ul>
+            </div>
+          </div>
+          <div id="market-space">
+            <img src={images[currentImage]} alt="marketers" />
           </div>
         </div>
-    
 
-
-        <div id="rightscreen">
-          <div id="currentroom">
-          <h3>Current Room</h3>
-          </div>
+        <div id="right-screen">
+          <div id="current-room">
+            <h3>{currentRoom}</h3>
           </div>
 
+          <div id="chat-box-and-buttons">
+            <div id="chat-box">
+              {messageData.map((message) => {
+                if (message.room === currentRoom) {
+                  return (
+                    <>
+                      <div className="message-input">
+                        <h2>
+                          <span>{message.user}</span> says:
+                        </h2>
+                        <p>{message.messageBody}</p>
+                        <h3>Time: {message.when}</h3>
+                      </div>
+                    </>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+            </div>
 
+            <div id="input-forms">
+              <form onSubmit={handleSubmit}>
+                <label>
+                  Username:
+                  <input
+                    id="username-input"
+                    type="text"
+                    name="user"
+                    value={input.user}
+                    onChange={handleChange}
+                  />
+                </label>
 
-          <div id="chatbox">
-          {messageData.map((message) => {
-              return (
-                <>
-                  <h1>----------</h1>
-                  <h2>Username: {message.user}</h2>
-                  <p>Message: {message.body}</p>
-                  <h1>----------</h1>
-                </>
-              );
-            })}
+                <label>
+                  Message:
+                  <input
+                    id="message-input"
+                    type="text"
+                    name="messageBody"
+                    value={input.messageBody}
+                    onChange={handleChange}
+                  />
+                </label>
+                <button type="submit" value="Submit">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
+        </div>
+      </div>
 
-          <div id="input-forms">
-          <form onSubmit={handleSubmit}>
-            <label>
-              Username:
-              <input
-                type="text"
-                name="user"
-                value={input.user}
-                onChange={handleChange}
-              />
-            </label>
-
-            <label>
-              Message:
-              <input
-                type="text"
-                name="messageBody"
-                value={input.messageBody}
-                onChange={handleChange}
-              />
-            </label>
-
-            <button type="submit" value="Submit">
-              Submit
-            </button>
-
-            <button type="submit" value="Submit">
-              Refresh
-            </button>
-
-          </form>
-          </div>
-
-          </main>
       <footer></footer>
-
-    </body>
-
+    </main>
   );
 }
 
--- server.js
+++ server.js
@@ -1,51 +1,40 @@
 const express = require("express");
 
-const port = process.env.PORT || 8000;
 const app = express();
+const port = process.env.PORT || 8000;
 const cors = require("cors");
 
 const mongoose = require('mongoose')
-
 mongoose.connect("mongodb://localhost:27017/messages");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 
+
+app.use(cors());
+app.use(express.json());
+
 const messageSchema = new mongoose.Schema({
-  when: Date,
   user: String,
-  room: String,
   messageBody: String,
+  room: String,
+  when: String,
 });
 
 const Message = mongoose.model("messages", messageSchema);
 
-
-app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-
 app.get("/get-all-messages", async (req, res) => {
-  let allMessages = await Message.find({});
+  let allMessages = await Message.find();
   res.send(allMessages);
 });
 
-function addMessage(){
-  let message = new Message({user: "Max", messageBody: "What's up"});
-  message.save();
-}
-
-addMessage();
-
-async function read(){
-  let allMessages = await Message.find({});
-  console.log(allMessages)
-}
-
-read()
-
 app.post("/add-message", async (req, res) => {
   let message = new Message(req.body);
+  let currentDate = new Date().toLocaleString();
   message.save();
+  await Message.updateOne(
+    {_id: message._id},
+    {$set: {when: currentDate}}
+  );
 });
 
 app.listen(port, () => {
